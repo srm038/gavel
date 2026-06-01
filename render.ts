da557e39e20a02563c33fcb230e4264c8b27079e
Shelby <srm038@gmail.com>
2026-06-01 17:13:28 -0500

    fix: ensure proper punctuation in report presentation

--- render.ts
+++ render.ts
@@ -138,7 +138,7 @@ for (const file of files) {
   if (m.reports?.length) {
     md(`## Reports`);
     for (const r of m.reports) {
-      let block = `- ${r.by} presented ${r.subject}.`;
+      let block = `- ${r.by} presented ${r.subject}${r.subject.endsWith(".") ? "" : "."}`;
       if (r.motions?.length) block += `\n\n${renderMotions(r.motions, "    ")}`;
       md(block);
     }
