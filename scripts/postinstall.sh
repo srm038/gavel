afa692342dfde6e44ab1909c504603d32ced5420
Shelby <srm038@gmail.com>
2026-07-29 17:53:18 -0500

    refactor(helix): symlink languages.toml from config dir
    Replace inline heredoc gen in postinstall with symlink
    to config/helix-languages.toml. Makes config trackable.
    
    Add committee.schema.yml to schema paths and package files.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 set -e
-if [ -n "$INIT_CWD" ] && [ ! -f "$INIT_CWD/.helix/languages.toml" ]; then
-  mkdir -p "$INIT_CWD/.helix"
-  cat > "$INIT_CWD/.helix/languages.toml" << 'HELIX'
-[[language]]
-name = "yaml"
-language-servers = ["yaml-language-server"]
 
-[language-server.yaml-language-server]
-command = "yaml-language-server"
+if [ -z "$INIT_CWD" ]; then exit 0; fi
 
-[language-server.yaml-language-server.config]
-yaml = { schemas = { "node_modules/gavel/schemas/minutes.schema.yml" = ["*.minutes.yml"], "node_modules/gavel/schemas/agenda.schema.yml" = ["*.agenda.yml"] } }
-HELIX
-fi
+mkdir -p "$INIT_CWD/.helix"
+ln -sf "$PWD/config/helix-languages.toml" "$INIT_CWD/.helix/languages.toml"
